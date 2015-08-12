2dee615dd749c9c60bde09a15243a9919689bafb
croussy <clroussy@laposte.net>
2015-08-12 09:26:09 +0200

    Add products route + add view json products

---
+++ app/controllers/seriously/v1/products_controller.rb
@@ -0,0 +1,7 @@
+# coding: utf-8
+class Seriously::V1::ProductsController < Seriously::V1::BaseController
+  def index
+    @products = Product.where(category_id: ProductNatureCategory.where(saleable: true)).availables
+
+  end
+end
